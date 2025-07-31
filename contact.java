130849197a0701942f65fdd87487e3721f0ab711
varsha02022006-max <varsha02022006@gmail.com>
2025-07-31 19:47:08 +0545

    Add files via upload

---
+++ contact.java
@@ -0,0 +1,52 @@
+
+
+
+EX NO:2 
+
+import java.util.ArrayList; 
+
+class contact 
+
+{ 
+
+public static void main(String[]varsha) 
+
+{ 
+
+ArrayList<String>con=new ArrayList<String>(); 
+
+con.add("varsha"); 
+
+con.add("renitha"); 
+
+con.add("priyanka"); 
+
+con.add("dharani"); 
+
+con.add("dharseka"); 
+
+for(String con1:con) 
+
+{ 
+
+System.out.println("contactList:"+con1); 
+
+} 
+
+System.out.println("Retriere:"+con.get(3)); 
+
+System.out.println("Update:"+con.set(4,"Rohini")); 
+
+System.out.println("Delete:"+con.remove(2)); 
+
+} 
+
+} 
+
+ 
+
+ 
+
+ 
+
+ 
